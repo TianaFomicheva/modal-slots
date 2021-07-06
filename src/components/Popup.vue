acfd76635f383a4b17fdfdb51dd770f7dc7cf032
tianaFomicheva <tianirika@gmail.com>
2021-07-06 23:33:50 +0500

    init + first steps

---
+++ src/components/Popup.vue
@@ -0,0 +1,89 @@
+<template>
+  <div v-if="isOpen" class="backdrop" @click="close">
+    <div class="popup" @click.stop>
+      <h1>Внимание!</h1>
+      <hr />
+      <slot></slot>
+      <hr />
+      <div class="footer">
+        <button @click="close">Отмена</button>
+        &nbsp;
+        <button @click="confirm">Ok</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: {
+    ok: null,
+    close: null,
+  },
+
+  mounted() {
+    document.addEventListener("keydown",(e)=>{
+      if(this.isOpen && e.key === 'Escape'){this.isOpen = false}
+    })
+  },
+  beforeUnmount() {
+    document.removeEventListener("keydown",(e)=>{
+      if(this.isOpen && e.key === 'Escape'){this.isOpen = false}
+    })
+  },
+  
+
+  methods: {
+    handleKeydown(e) {
+      if (this.isOpen && e.key === "Escape") {
+        this.close();
+      }
+    },
+
+    close() {
+      this.$emit("close");
+    },
+    confirm() {
+      this.$emit("ok");
+    },
+  },
+};
+</script>
+
+<style>
+.popup {
+  top: 50px;
+  padding: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  position: fixed;
+  z-index: 101;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.popup h1 {
+  text-align: center;
+  margin: 0;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 100;
+}
+
+.footer {
+  text-align: right;
+}
+</style>
